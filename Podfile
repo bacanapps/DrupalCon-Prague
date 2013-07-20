2c4c78fbb9bcd5e8f86e4525b381610c9d547225
Tim Leytens <leytens.tim@gmail.com>
2013-07-20 09:02:04 +0200

    Podfile Afnetworking SDurlCache GA

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '5.0'
+pod 'AFNetworking', '~> 1.2.0'
+pod 'SDURLCache', '~> 1.2'
+pod 'GoogleAnalytics-iOS-SDK', '~> 2.0beta4'
+
+
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,27 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 1
+#define COCOAPODS_VERSION_MINOR_AFNetworking 2
+#define COCOAPODS_VERSION_PATCH_AFNetworking 1
+
+// GoogleAnalytics-iOS-SDK
+#define COCOAPODS_POD_AVAILABLE_GoogleAnalytics_iOS_SDK
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 2.0beta4.
+
+// SDURLCache
+#define COCOAPODS_POD_AVAILABLE_SDURLCache
+#define COCOAPODS_VERSION_MAJOR_SDURLCache 1
+#define COCOAPODS_VERSION_MINOR_SDURLCache 2
+#define COCOAPODS_VERSION_PATCH_SDURLCache 0
+
